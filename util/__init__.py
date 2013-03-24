7b78857b0bc0bf3d3d54afd2baa79a8def197744
mckelvin <ibmmc@live.com>
2013-03-24 20:20:47 +0800

    add util to get SSL cert. subject common name(CN)

---
+++ util/__init__.py
@@ -0,0 +1,35 @@
+# coding: UTF-8
+
+import sys
+
+from M2Crypto import SSL
+
+def get_ssl_subject_CN(ip_address,ssl_port=443):
+    ctx = SSL.Context()
+    ctx.set_allow_unknown_ca(True)
+    ctx.set_verify(SSL.verify_none, 1)
+    conn = SSL.Connection(ctx)
+    conn.postConnectionCheck = None
+    timeout = SSL.timeout(1)
+    conn.set_socket_read_timeout(timeout)
+    conn.set_socket_write_timeout(timeout)
+    try:
+      conn.connect((ip_address,ssl_port))
+    except Exception, e:
+        #print 'ssl conn error:',e
+        return
+
+    cert = conn.get_peer_cert()
+    try:
+        c_CN = cert.get_subject().CN
+    except AttributeError:
+        c_CN = ""
+    try:
+       c_AltName = cert.get_ext('subjectAltName').get_value()
+    except AttributeError:
+        c_AltName = ""
+    conn.close()
+    return c_CN
+
+if __name__ == '__main__':
+    assert get_ssl_subject_CN('203.208.36.1') == '*.google.com'
